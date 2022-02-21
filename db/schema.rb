36b1291acb6f37c21a4ce458ca683d119e250184
Karel van Oordt <vanoordtkarel@gmail.com>
2022-02-21 11:24:09 -0500

    Fix create_user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_17_143141) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_21_153531) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,6 +45,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_17_143141) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "photo"
+    t.text "bio"
+    t.integer "posts_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
