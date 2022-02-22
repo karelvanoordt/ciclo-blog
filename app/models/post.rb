51de5afec444b28db962dae45f6772734fb9adc4
Karel van Oordt <vanoordtkarel@gmail.com>
2022-02-22 03:57:34 -0500

    Add methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,15 @@ class Post < ApplicationRecord
     belongs_to :author, class_name: 'User'
     has_many :comments
     has_many :likes
+
+    def show_recent_5_comments
+        comment.order('created_at Desc').limit(5)
+    end
+
+    after_save :update_post_counter
+
+    def update_post_counter
+        user.increment!(:posts_counter)
+    end
+    
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
     has_many :posts, foreign_key: 'user_id'
     has_many :comments, foreign_key: 'user_id'
     has_many :likes, foreign_key: 'user_id'
+
+    def show_recent_3_posts
+        Post.last(3)
+    end
 end
