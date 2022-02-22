7131fc59f2acba563d63312f3aaa89a686d95d7c
Karel van Oordt <vanoordtkarel@gmail.com>
2022-02-22 03:58:01 -0500

    Fix linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-    has_many :posts, foreign_key: 'user_id'
-    has_many :comments, foreign_key: 'user_id'
-    has_many :likes, foreign_key: 'user_id'
+  has_many :posts, foreign_key: 'user_id'
+  has_many :comments, foreign_key: 'user_id'
+  has_many :likes, foreign_key: 'user_id'
 
-    def show_recent_3_posts
-        Post.last(3)
-    end
+  def show_recent_3_posts
+    Post.last(3)
+  end
 end
