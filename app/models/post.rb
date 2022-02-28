faf0944e2fc481256709ac007e452046d0cd4eee
Karel van Oordt <vanoordtkarel@gmail.com>
2022-02-28 05:37:29 -0500

    Fix linters

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,7 @@
 class Post < ApplicationRecord
-    belongs_to :user
-    has_many :comments
-    has_many :likes
-
+  belongs_to :user
+  has_many :comments
+  has_many :likes
 
   def recent_5_comments
     Comment.last(5)
