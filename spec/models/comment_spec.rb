9c5c967f81c61a10765d48355147c7c42e4a6fea
Karel van Oordt <vanoordtkarel@gmail.com>
2022-03-02 18:10:48 -0500

    Fix linters

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,17 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
+  describe 'Comment model tests' do
+    subject { Comment.new(text: 'Text test', user_id: 1, post_id: 1) }
 
-    describe 'Comment model tests' do
-        subject { Comment.new(text: 'Text test', user_id: 1, post_id: 1) }
-        
-        before { subject.save }
+    before { subject.save }
 
-        it 'validates if update_comments_counter increases count' do
-            before_counter = Post.find(1).comments_counter
-            subject.update_comments_counter
-            expect(Post.find(1).comments_counter).to eq(before_counter + 1)
-        end
-
+    it 'validates if update_comments_counter increases count' do
+      before_counter = Post.find(1).comments_counter
+      subject.update_comments_counter
+      expect(Post.find(1).comments_counter).to eq(before_counter + 1)
     end
+  end
 end
