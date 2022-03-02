b10ac04f31e429690610fff0d4bc3140a699e882
Karel van Oordt <vanoordtkarel@gmail.com>
2022-03-02 18:10:17 -0500

    Update like and comment model tests / bug persists

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,17 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe Comment, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe Comment, type: :model do
+
+    describe 'Comment model tests' do
+        subject { Comment.new(text: 'Text test', user_id: 1, post_id: 1) }
+        
+        before { subject.save }
+
+        it 'validates if update_comments_counter increases count' do
+            before_counter = Post.find(1).comments_counter
+            subject.update_comments_counter
+            expect(Post.find(1).comments_counter).to eq(before_counter + 1)
+        end
+
+    end
+end
